345d03878ccdf50f6b536cb8d207a4b7f343f6ca
arif <arif@themascoteers.com>
2023-10-28 10:46:15 +1100

    done

--- analyze.py
+++ analyze.py
@@ -7,16 +7,17 @@ def plot_tensor(tensor):
     tensor = tensor.data.clone()
     tensor[tensor >= mean] = 255
     tensor[tensor < mean] = 0
-    plt.imshow(tensor.numpy(), cmap="hot")
+    plt.imshow(tensor.cpu().numpy(), cmap="hot")
     plt.show()
 
 
 def plot_filters(filters):
     filters = filters.clone()
     filters = filters.reshape(filters.shape[0], filters.shape[2], filters.shape[3])
+    filters = filters[0:2]
     for tensor in filters:
         plot_tensor(tensor)
 
 def analyze(model):
-    filters = model.net[0].weight.data
+    filters = model.layer1[0].weight.data
     plot_filters(filters)
